306baf76b8ed8fa3998f895bff12f3aa1752ddac
Hongyi23 <desperatev00@gmail.com>
2015-09-19 13:23:22 -0400

    finish book controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'books/new'
+  get 'addbook' => 'books#new'   #具名路由 让route产生addbook_path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
