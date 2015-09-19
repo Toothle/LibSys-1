36484cdf00102bf4298db594a9cbed034957263b
Olivia Huang <oliviahyacinth@gmail.com>
2015-09-19 10:08:22 -0400

    members model established

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'members/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
