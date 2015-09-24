b0142eea2060df47f63d30b00c0f72b1af3351a0
Olivia Huang <oliviahyacinth@gmail.com>
2015-09-23 20:03:05 -0400

    start checkout and return feature

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,7 +40,15 @@ class BooksController < ApplicationController
     flash[:success] = "Book deleted"
     redirect_to books_url
   end
-
+  
+  def checkout
+   @book =  Book.find(params[:id])
+   if @book.update_attributes(:status=>params[:status])
+      flash[:success] = "Book checked out"
+   end
+   redirect_to @book
+  end
+  
   def book_params
     params.require(:book).permit(:ISBN, :title, :author, :description, :status)
   end
